ead380e58b5f992f2d4d96e29429be4743ef74ed
Iván Monte <ivanmonte07.93@gmail.com>
2023-02-08 11:09:28 -0300

    chamber home created -Only  main, header, footer.

---
+++ Chamber/scripts/scripts.js
@@ -0,0 +1,67 @@
+//Burger Menu
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("burgerBox").classList.toggle("open");
+    document.getElementById("headerLogo").classList.toggle("close");
+    document.getElementById("headerTextInfo").classList.toggle("close");
+    document.getElementById("social-logo-box").classList.toggle("close");
+    document.getElementById("head-date-holder").classList.toggle("close");
+}
+
+const h = document.getElementById("hamburgerBtn");
+
+h.onclick = toggleMenu;
+
+
+
+//Header Date
+const headerDate = document.querySelector("#headerTime");
+const now = new Date();
+const headFullDate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+headerDate.innerHTML = `<em>${headFullDate}</em>`;
+
+
+//Footer Date
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+let d = new Date(document.lastModified);
+let year = d.getFullYear();
+let fulldate = `Last Updated: ${d.getMonth()+1}/${d.getDate()}/${year} ${minTwoDigits(d.getHours())}:${minTwoDigits(d.getMinutes())}:${minTwoDigits(d.getSeconds())}`
+const copyyears = document.querySelectorAll(".copyyear");
+const currentdates = document.querySelectorAll(".currentdate")
+
+var i;
+for (i = 0; i < copyyears.length; i++) {
+  copyyears[i].textContent= year;
+  currentdates[i].textContent = fulldate;
+}
+
+function minTwoDigits(n) {
+    return (n < 10 ? '0' : '') + n;
+}
