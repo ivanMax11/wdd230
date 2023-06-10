80732ab591d2dce66c6dede9aa5c97fb7ce859bc
Iván Maximiliano Monte <ivanmonte07.93@gmail.com>
2023-06-10 17:49:39 -0300

    fixed

--- week1/homepage/scripts/getDates.js
+++ week1/homepage/scripts/getDates.js
@@ -1,5 +1,24 @@
 
+// Obtener elementos del DOM
 const lastModifiedElement = document.getElementById('lastModified');
-const lastModified = document.lastModified;
+const yearElement = document.getElementById('year');
 
-lastModifiedElement.textContent = lastModified;
+// Obtener la fecha y hora de la última modificación
+const lastModified = new Date(document.lastModified).toLocaleString();
+
+// Obtener el año actual
+const currentYear = new Date().getFullYear();
+
+// Actualizar el contenido de los elementos
+lastModifiedElement.textContent = `Last modified: ${lastModified}`;
+yearElement.textContent = currentYear;
+
+
+// Obtener el elemento por su ID
+const flagIconElement = document.getElementById('flagIcon');
+
+// Asignar el contenido o estilo deseado al elemento
+flagIconElement.innerHTML = '🇦🇷'; // Puedes usar el código del emoji de la bandera de Argentina
+
+// Opcionalmente, puedes aplicar estilos al elemento
+flagIconElement.style.fontSize = '24px';
