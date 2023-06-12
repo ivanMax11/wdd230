50f2fe7a29e45df28e80e9df0a5f799c142994ab
Iván Maximiliano Monte <ivanmonte07.93@gmail.com>
2023-06-12 16:45:25 -0300

    corrections

---
+++ scripts/getDates.js
@@ -0,0 +1,14 @@
+// To get DOM element
+const lastModifiedElement = document.getElementById('lastModified');
+const yearElement = document.getElementById('year');
+
+// To get the date of last modification
+const lastModified = new Date(document.lastModified).toLocaleString();
+
+// To get the current year
+const currentYear = new Date().getFullYear();
+
+// Update the content of elements
+lastModifiedElement.textContent = `Last modified: ${lastModified}`;
+yearElement.textContent = currentYear;
+
