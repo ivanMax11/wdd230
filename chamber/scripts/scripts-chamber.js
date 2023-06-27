adeafb9d14065f8742cc7e9c5f5d86b71b7140ca
Iván Maximiliano Monte <ivanmonte07.93@gmail.com>
2023-06-27 02:07:00 -0300

    Discover page added

--- chamber/scripts/scripts-chamber.js
+++ chamber/scripts/scripts-chamber.js
@@ -19,3 +19,39 @@ menuBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   menuBtn.classList.toggle('active');
 });
+
+
+
+// Obtener el elemento del sidebar
+
+var sidebar = document.getElementById('sidebar-box6');
+
+// Obtener la fecha actual
+var currentDate = new Date();
+
+// Obtener la última fecha de visita almacenada en localStorage
+var lastVisitDate = localStorage.getItem('lastVisitDate');
+
+if (lastVisitDate) {
+  // Calcular la diferencia de tiempo entre la última visita y la fecha actual
+  var timeDiff = currentDate.getTime() - new Date(lastVisitDate).getTime();
+
+  // Convertir la diferencia de tiempo a días
+  var daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+  if (daysDiff === 0) {
+    // Mostrar mensaje si la última visita fue hoy
+    sidebar.innerHTML = ' 📅 Back so soon! Awesome!  🚀';
+  } else {
+    // Mostrar mensaje con la cantidad de días desde la última visita
+    var message = (daysDiff === 1) ? 'day' : 'days';
+    sidebar.innerHTML = 'You last visited ' + daysDiff + ' ' + message + ' ago. 😊';
+  }
+} else {
+  // Mostrar mensaje si es la primera visita del usuario
+  sidebar.innerHTML = ' 📅 Welcome! Let us know if you have any questions. 🎉';
+}
+
+// Guardar la fecha de visita actual en localStorage
+localStorage.setItem('lastVisitDate', currentDate);
+
