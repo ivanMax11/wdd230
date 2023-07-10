2542bae0b73b585c9fb3ed12c1c57c3e5f8fd4a8
Iván Maximiliano Monte <ivanmonte07.93@gmail.com>
2023-07-10 13:14:27 -0300

    corrections in links with json

--- scripts/links.js
+++ scripts/links.js
@@ -2,10 +2,33 @@ const baseURL = "https://ivanmax11.github.io/wdd30/";
 const linkURL = "https://ivanmax11.github.io/wdd30/data/links.json";
 
 async function getLink() {
-    const response = await fetch (linkURL);
-    const data = await response.json();
-    displayLinks(data);
-    console.log(data);
+    try {
+        const response = await fetch(linkURL);
+        const data = await response.json();
+        displayLinks(data.weeks);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displayLinks(weeks) {
+    weeks.forEach((week) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${week.weeks}: `;
+
+        week.links.forEach((link) => {
+            const linkElement = document.createElement("a");
+            linkElement.href = baseURL + link.url;
+            linkElement.textContent = link.title;
+            listItem.appendChild(linkElement);
+
+            const separator = document.createTextNode(" | ");
+            listItem.appendChild(separator);
+    });
+
+    activitiesList.appendChild(listItem);
+});
+
 }
 
 getLink();
